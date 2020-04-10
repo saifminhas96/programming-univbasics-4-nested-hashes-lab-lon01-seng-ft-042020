334d8ab67db3c95df897b808a4dee548bf99b9c8
Saif Minhas <saif_minhas@hotmail.com>
2020-04-10 10:34:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -17,7 +17,7 @@ def hopper
     }
 
 end
-grace_hopper = 
+grace_hopper = programmer_hash
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
