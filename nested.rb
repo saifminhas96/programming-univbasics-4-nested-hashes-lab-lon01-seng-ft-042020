4897837a3b3d3c409679a5a1a6589acdb946b629
Saif Minhas <saif_minhas@hotmail.com>
2020-04-10 10:44:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,12 +61,7 @@ programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
+
 
 	programmer_hash = 
  		
